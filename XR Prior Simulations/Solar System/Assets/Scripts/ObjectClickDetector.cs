8c4f65ee25dee90ddfd368d01d745e3bfd8b7c3f
Gina <ginaelliott4242@gmail.com>
2026-07-06 15:51:56 +0100

    Clicking planet functionality (incomplete)

---
+++ XR Prior Simulations/Solar System/Assets/Scripts/ObjectClickDetector.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectClickDetector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OnObjectClicked() {
+        Debug.Log("The object has been clicked");
+    }
+}
