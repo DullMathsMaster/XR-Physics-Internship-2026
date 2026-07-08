a661b7d22e3fa474cfee43ab7b64c9e1b98433de
Gina <ginaelliott4242@gmail.com>
2026-07-08 16:46:54 +0100

    Updated functionality on seasons button

--- XR Prior Simulations/Solar System/Assets/Scripts/CanvasManager.cs
+++ XR Prior Simulations/Solar System/Assets/Scripts/CanvasManager.cs
@@ -1,21 +1,28 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CanvasManager : MonoBehaviour
 {
     public Canvas earthUICanvas;
     public Canvas earthSeasonsCanvas;
+    public Slider targetSlider;
 
     public void ToggleCanvases()
     {
         if (earthUICanvas != null)
         {
-            // If it's enabled, this disables it. If disabled, it enables it.
             earthUICanvas.enabled = !earthUICanvas.enabled;
         }
 
         if (earthSeasonsCanvas != null)
         {
             earthSeasonsCanvas.enabled = !earthSeasonsCanvas.enabled;
+
+            // Check if earthSeasonsCanvas was just turned OFF (false)
+            if (!earthSeasonsCanvas.enabled && targetSlider != null)
+            {
+                targetSlider.value = 500f; 
+            }
         }
     }
 }
