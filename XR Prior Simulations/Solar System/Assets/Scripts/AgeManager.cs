255cc450ad9c90d80df6309f6daa7e2b744c53a6
jtucker3 <149870614+jtucker3@users.noreply.github.com>
2026-07-13 16:49:23 +0100

    Update AgeManager.cs: push specific changes

---
+++ XR Prior Simulations/Solar System/Assets/Scripts/AgeManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public enum AgeGroup
+{
+    KS2,
+    KS3,
+    KS4,
+    KS5,
+}
+
+public class AgeManager : MonoBehaviour
+{
+    public static AgeManager Instance;
+
+    public AgeGroup CurrentAge = AgeGroup.KS2;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+}
